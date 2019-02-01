ecc9eee359c87012f7f03ef8d87d3e174c868b13
Jsonghh <jiesonghe@outlook.com>
2019-02-01 01:10:24 -0500

    HOC_start

---
+++ pages/secret.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import BaseLayout from '../components/layouts/BaseLayout';
+import BasePage from '../components/BasePage';
+import withAuth from '../components/hoc/withAuth';
+
+
+class Secret extends React.Component {
+
+  // renderSecretPage() {
+  //   const {isAuthenticated} = this.props.auth;
+  //   if (isAuthenticated) {
+  //     return (
+  //       <BaseLayout {...this.props.auth}>
+  //         <BasePage>
+  //           <h1> I am Secret Page</h1>
+  //           <p> Secret Content Here </p>
+  //         </BasePage>
+	// 		  </BaseLayout>
+  //     )
+  //   } else {
+  //     return (
+  //       <BaseLayout {...this.props.auth}>
+  //         <BasePage>
+  //           <h1> You are not authenticated</h1>
+  //         </BasePage>
+  //       </BaseLayout>
+  //     )
+  //   }
+  // }
+
+	render() {
+    debugger;
+    console.log(this.props)
+    return (
+      <BaseLayout {...this.props.auth}>
+      <BasePage>
+        <h1> I am Secret Page</h1>
+        <p> Secret Content Here </p>
+      </BasePage>
+    </BaseLayout>
+    )
+	}
+}
+
+export default withAuth(Secret);
--- services/auth0.js
+++ services/auth0.js
@@ -79,10 +79,11 @@ class Auth0 {
   async verifyToken(token) {
     if (token) {
       const decodedToken = jwt.decode(token, {complete: true});
+      if (!decodedToken) {return undefined;}
       const jwks = await this.getJWKS();
       const jwk = jwks.keys[0];
       // build certificate
-      let cert = jwk.x5c[0];
+      let cert = jwk.x5c[0]; 
       cert = cert.match(/.{1,64}/g).join('\n');
       cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
 
